4b7b9b985116cbce879280312fd3ec57b7dfeb3e
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-05-09 22:40:27 +0300

    set metadata naming convention

--- fastapi-application/core/models/base.py
+++ fastapi-application/core/models/base.py
@@ -1,13 +1,19 @@
+from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import declared_attr
 
+from core.config import settings
 from utils import camel_case_to_snake_case
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
+
+    metadata = MetaData(
+        naming_convention=settings.db.naming_convention,
+    )
 
     @declared_attr.directive
     def __tablename__(cls) -> str:
