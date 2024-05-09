77fb4a5cb1bd942b02e480a52fde9302e44dc944
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-05-09 22:08:02 +0300

    add naming convention for models

--- fastapi-application/core/config.py
+++ fastapi-application/core/config.py
@@ -22,6 +22,14 @@ class DatabaseConfig(BaseModel):
     pool_size: int = 50
     max_overflow: int = 10
 
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
