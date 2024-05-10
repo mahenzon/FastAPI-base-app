2d2bac139cc34e7e7b1688851f7c8df71903ec6d
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-05-10 12:03:47 +0300

    add api prefix config

--- fastapi-application/core/config.py
+++ fastapi-application/core/config.py
@@ -11,8 +11,13 @@ class RunConfig(BaseModel):
     port: int = 8000
 
 
+class ApiV1Prefix(BaseModel):
+    prefix: str = "/v1"
+
+
 class ApiPrefix(BaseModel):
     prefix: str = "/api"
+    v1: ApiV1Prefix = ApiV1Prefix()
 
 
 class DatabaseConfig(BaseModel):
