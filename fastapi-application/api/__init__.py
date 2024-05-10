2efd65fced3e5a97cd1f02e44a7352729650733b
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-05-10 12:04:21 +0300

    register api v1 router

--- fastapi-application/api/__init__.py
+++ fastapi-application/api/__init__.py
@@ -1,3 +1,9 @@
 from fastapi import APIRouter
 
-router = APIRouter()
+from core.config import settings
+from .api_v1 import router as router_api_v1
+
+router = APIRouter(
+    prefix=settings.api.prefix,
+)
+router.include_router(router_api_v1)
--- fastapi-application/main.py
+++ fastapi-application/main.py
@@ -24,7 +24,6 @@ main_app = FastAPI(
 )
 main_app.include_router(
     api_router,
-    prefix=settings.api.prefix,
 )
 
 if __name__ == "__main__":
