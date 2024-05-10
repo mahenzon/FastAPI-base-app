f78ae92d26bc1641625add5d26166388fe607832
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-05-10 12:08:51 +0300

    Create User SQLA model

--- fastapi-application/core/models/__init__.py
+++ fastapi-application/core/models/__init__.py
@@ -1,7 +1,9 @@
 __all__ = (
     "db_helper",
     "Base",
+    "User",
 )
 
 from .db_helper import db_helper
 from .base import Base
+from .user import User
---
+++ fastapi-application/core/models/user.py
@@ -0,0 +1,16 @@
+from sqlalchemy import UniqueConstraint
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+from .base import Base
+from .mixins.int_id_pk import IntIdPkMixin
+
+
+class User(IntIdPkMixin, Base):
+    username: Mapped[str] = mapped_column(unique=True)
+    foo: Mapped[int]
+    bar: Mapped[int]
+
+    __table_args__ = (
+        UniqueConstraint("foo", "bar"),
+    )
