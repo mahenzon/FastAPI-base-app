e196be202ffc312653695153501730a245745f01
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-08-30 23:02:46 +0300

    add CreatedAtMixin to User model

--- fastapi-application/core/models/user.py
+++ fastapi-application/core/models/user.py
@@ -3,9 +3,10 @@ from sqlalchemy.orm import mapped_column
 
 from .base import Base
 from .mixins.int_id_pk import IntIdPkMixin
+from .mixins.created_at import CreatedAtMixin
 
 
-class User(IntIdPkMixin, Base):
+class User(IntIdPkMixin, CreatedAtMixin, Base):
     username: Mapped[str] = mapped_column(unique=True)
 
     @property
